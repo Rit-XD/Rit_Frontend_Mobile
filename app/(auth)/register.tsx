da35e3cd34cef8ca9e0db92937825af3e45cec89
Senne <>
2024-05-29 18:31:39 +0200

    update regex

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -60,13 +60,12 @@ const SignUpScreen = () => {
   }
 
   function isValidEmail(email: string) {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,63}$/;
     return re.test(email);
   }
 
   function isValidPhoneNumber(phone: string) {
-    const re = /^(\+32\s|0)4\s?\d{2}\s?\d{2}\s?\d{2}\s?\d{2}$/;
+    const re = /^(0|\+32)\s?4\s?70\s?42\s?93\s?61$/;
     return re.test(phone);
   }
 
