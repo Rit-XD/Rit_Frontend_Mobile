93d26be5a2d01ef91520140921665e9643e88ba5
Bram-Colleman <r0820751@student.thomasmore.be>
2024-06-03 22:57:43 +0200

    list rides

--- src/components/rides/RidesList.tsx
+++ src/components/rides/RidesList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { ThemedView } from "../ThemedView";
 import { ThemedText } from "../ThemedText";
@@ -13,21 +13,28 @@ export default function RidesList({filter}: AvailabilityProps) {
     const { availableRides, acceptedRides } = useAuth();
     const color = useThemeColor({ light: "#fefefe", dark: "#fff" }, 'background');
 
-    useEffect(() => {
-        console.log(availableRides);
-        console.log(acceptedRides);
-    }, [availableRides, acceptedRides]);
-
     return (
         <View style={styles.container}>
-            <ThemedView style={styles.rideContainer}>
+            <ScrollView>
+                    {availableRides.length && (
+                        <>
+                            {availableRides.map((ride) => (
+                                <ThemedView style={styles.rideContainer}>
+                                    <ThemedText>{ride.destination}</ThemedText>
+                                </ThemedView>
+                            ))}
+                        </>
+                    )}
+            </ScrollView>
+
+
+            {/* <ThemedView style={styles.rideContainer}>
             {availableRides.length? (
-                <ThemedText>{availableRides[0].origin}</ThemedText>
+                <ThemedText>Geen ritten</ThemedText>
             ) : (
                 <ThemedText>Geen ritten</ThemedText>
             )}
-            
-            </ThemedView>
+            </ThemedView> */}
 
         </View>
     );
@@ -46,7 +53,7 @@ const styles = StyleSheet.create({
         shadowColor: "rgba(0, 0, 0, 0.25)",
         shadowOffset: { width: 0, height: 1 },
         shadowRadius: 9,
-        shadowOpacity: .5,
+        shadowOpacity: 1,
         borderRadius: 15,
         marginHorizontal: 16,
         marginVertical: 4,
