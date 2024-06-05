4fe3461deb0662bdef9ded2818e719b88c2477c8
Senne <>
2024-06-05 13:18:47 +0200

    add asset bundle patterns

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,57 +1,66 @@
-import { SplashScreen, Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useEffect } from 'react';
-import { ScrollView, View, useColorScheme } from 'react-native';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import AuthProvider from '@/providers/AuthProvider';
-
-
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useEffect } from "react";
+import { ScrollView, View, useColorScheme } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import AuthProvider from "@/providers/AuthProvider";
 
 export {
-    // Catch any errors thrown by the Layout component.
-    ErrorBoundary,
-  } from 'expo-router';
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
-  export const unstable_settings = {
-    // Ensure that reloading on `/modal` keeps a back button present.
-    initialRouteName: '(tabs)',
-  };
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
 
-  // Prevent the splash screen from auto-hiding before asset loading is complete.
-  SplashScreen.preventAutoHideAsync();
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
-  export default function RootLayout() {
-    const [loaded, error] = useFonts({
-        // Load a custom font from a URL.
-        'Cocon': require('@assets/fonts/Cocon.otf'),
-        ...FontAwesome.font,
-        });
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    // Load a custom font from a URL.
+    Cocon: require("@assets/fonts/Cocon.otf"),
+    ...FontAwesome.font,
+  });
 
-        useEffect(() => {
-            if(error) throw error;
-        }, [error]);
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
-        useEffect(() => {
-            if(loaded) SplashScreen.hideAsync();
-        }, [loaded]);
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
 
-        if(!loaded) return null;
+  if (!loaded) return null;
 
-        return <RootLayoutNav />
-  }
+  return <RootLayoutNav />;
+}
 
-  function RootLayoutNav() {
-    const colorscheme = useColorScheme();
+function RootLayoutNav() {
+  const colorscheme = useColorScheme();
 
-    return (
-        <AuthProvider>
-            <ThemeProvider value={colorscheme === "dark"? DarkTheme : DefaultTheme}>
-                    <Stack screenOptions={{ headerShown: false, contentStyle: {backgroundColor: colorscheme === "dark"? "#151515" : "#ffffff"}}} >
-                        <Stack.Screen name="(auth)"/>
-                        <Stack.Screen name="(tabs)"/>
-                    </Stack>
-            </ThemeProvider>
-        </AuthProvider>
-    )
-  }
+  return (
+    <AuthProvider>
+      <ThemeProvider value={colorscheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: colorscheme === "dark" ? "#151515" : "#ffffff",
+            },
+          }}
+        >
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+    </AuthProvider>
+  );
+}
