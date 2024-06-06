46968fdeeea39400e3f4f1341532de69bd120b51
Bram-Colleman <r0820751@student.thomasmore.be>
2024-06-06 15:01:20 +0200

    Home ridelist layout +styling

--- src/components/rides/RidesList.tsx
+++ src/components/rides/RidesList.tsx
@@ -4,52 +4,89 @@ import { useThemeColor } from "@/hooks/useThemeColor";
 import { ThemedView } from "../ThemedView";
 import { ThemedText } from "../ThemedText";
 import { useAuth } from "@/providers/AuthProvider";
+import { Ride } from "@/types/Ride.type";
+import { primaryColor } from "@/constants/Colors";
+import { color } from "@rneui/themed/dist/config";
 
-type AvailabilityProps = {
-  filter: "all" | "accepted";
-};
 
-export default function RidesList({filter}: AvailabilityProps) {
+
+export default function RidesList() {
     const { availableRides, acceptedRides } = useAuth();
     const color = useThemeColor({ light: "#fefefe", dark: "#fff" }, 'background');
+    const [filter, setFilter] = React.useState<"all" | "accepted">("all");
 
-    return (
-        <View style={styles.container}>
-            <ScrollView>
+    if (filter === "all") {
+        return (
+            <View style={styles.container}>
+                <View style={{display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-around", paddingHorizontal: 24}}>
+                    <View style={{ borderBottomColor: primaryColor, borderBottomWidth: 4}}>
+                        <ThemedText style={{paddingTop: 24,paddingBottom: 4, paddingHorizontal: 8, fontSize: 22, color: primaryColor}}>Aanvragen</ThemedText>
+                    </View>
+                    <ThemedText style={{paddingTop: 24,paddingBottom: 4, paddingHorizontal: 8, fontSize: 22}} onPress={() => {setFilter("accepted")}}>Ritten</ThemedText>
+    
+                </View>
+                <ScrollView>
                     {availableRides.length && (
                         <>
-                            {availableRides.map((ride) => (
-                                <ThemedView style={styles.rideContainer}>
-                                    <ThemedText>{ride.destination}</ThemedText>
-                                </ThemedView>
-                            ))}
+                        {availableRides.map((ride) => (
+                            <ThemedView style={styles.rideContainer} key={ride.id}>
+                                <View style={styles.rideContainerInner}>
+                                    <View style={styles.colorCode}></View>
+                                    <View style={styles.content}>
+                                        <ThemedText>{ride.origin}</ThemedText>
+                                        <ThemedText>{ride.destination}</ThemedText>
+                                    </View>
+                                </View>
+                            </ThemedView>
+                        ))}
                         </>
                     )}
-            </ScrollView>
-
-
-            {/* <ThemedView style={styles.rideContainer}>
-            {availableRides.length? (
-                <ThemedText>Geen ritten</ThemedText>
-            ) : (
-                <ThemedText>Geen ritten</ThemedText>
-            )}
-            </ThemedView> */}
-
-        </View>
-    );
+                </ScrollView>
+            </View>
+        );
+    } else {
+        return (
+            <View style={styles.container}>
+                <View style={{display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-around", paddingHorizontal: 24}}>
+                    <ThemedText style={{paddingTop: 24,paddingBottom: 4, paddingHorizontal: 8, fontSize: 22}} onPress={() => {setFilter("all")}}>Aanvragen</ThemedText>
+                    <View style={{borderBottomColor: primaryColor, borderBottomWidth: 4}}>
+                        <ThemedText style={{paddingTop: 24,paddingBottom: 4, paddingHorizontal: 8, fontSize: 22, color: primaryColor}}>Ritten</ThemedText>
+                    </View>
+                </View>
+                <ScrollView>
+                    {acceptedRides.length && (
+                        <>
+                        {acceptedRides.map((ride) => (
+                            <ThemedView style={styles.rideContainer} key={ride.id}>
+                                <View style={styles.rideContainerInner}>
+                                    <View style={styles.colorCode}></View>
+                                    <View style={styles.content}>
+                                        <ThemedText>{ride.origin}</ThemedText>
+                                        <ThemedText>{ride.destination}</ThemedText>
+                                    </View>
+                                </View>
+                            </ThemedView>
+                        ))}
+                        </>
+                    )}
+                </ScrollView>
+            </View>
+        );
+    }
 };
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        marginTop: -16,
     },
     rideContainer: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: 16,
+        height: 64,
+        padding: 0,
         shadowColor: "rgba(0, 0, 0, 0.25)",
         shadowOffset: { width: 0, height: 1 },
         shadowRadius: 9,
@@ -57,5 +94,24 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         marginHorizontal: 16,
         marginVertical: 4,
+        position: "relative",
     },
+    rideContainerInner: {
+        overflow: "hidden",
+        height: "100%",
+        borderRadius: 15,
+    },
+    content: {
+        backgroundColor: "transparent",
+        marginLeft: 32,
+        height: "100%",
+        justifyContent: "center",
+    },
+    colorCode: {
+        position: "absolute",
+        width: 8,
+        height: "100%",
+        backgroundColor: primaryColor,
+    },
+
 });
