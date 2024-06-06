e11d02336952b0594f3992afc2bfda80120eb410
Bram-Colleman <r0820751@student.thomasmore.be>
2024-06-06 22:21:30 +0200

    add rideDetails

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,13 +1,24 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { forwardRef } from "react";
 import { Colors, primaryColor } from "@/constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
 
 type ButtonProps = {
   text: string;
+  icon?: string;
+  family?: "antdesign";
+  mod?: ["white"?, "square"?]; 
 } & React.ComponentPropsWithoutRef<typeof Pressable>;
 
 const Button = forwardRef<View | null, ButtonProps>(
-  ({ text, ...pressableProps }, ref) => {
+  ({ text, icon, family, mod, ...pressableProps }, ref) => {
+    if (icon && family) {
+      if(family === "antdesign") return (
+        <Pressable ref={ref} {...pressableProps} style={[styles.container, mod?.includes("white")? {backgroundColor: "white"}: {}, mod?.includes("square")? {borderRadius: 15}: {}]}>
+          <AntDesign name={icon} size={24} color={mod?.includes("white")? "#151515": "white"} />
+        </Pressable>
+      )
+    }
     return (
       <Pressable ref={ref} {...pressableProps} style={styles.container}>
         <Text style={styles.text}>{text}</Text>
