f1e3d0874b267d2e0cd20c39ccb35fc155794bd2
Bram-Colleman <r0820751@student.thomasmore.be>
2024-06-12 20:02:43 +0200

    setup stream chat ui

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,85 +1,110 @@
 import { Redirect, Tabs } from "expo-router";
-import React from "react";
-
+import React, { useEffect } from "react";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { useAuth } from "@/providers/AuthProvider";
+import { StreamChat } from "stream-chat";
+import { Chat, OverlayProvider } from "stream-chat-expo";
+
+const client = StreamChat.getInstance("zpg78zdqbyrd");
 
 export default function TabLayout() {
   const {colorScheme} = useAuth();
   const { session } = useAuth();
 
+  useEffect(() => {
+    const connect = async() => {
+      await client.connectUser(
+        {
+          id: 'jlahey',
+          name: 'Jim Lahey',
+          image: 'https://i.imgur.com/fR9Jz14.png',
+        },
+        client.devToken('jlahey'),
+      );
+
+      // const channel = client.channel('messaging', 'the_park', {
+      //   name: 'The Park',
+      // });
+      // await channel.watch();
+    }
+    if(client.user === undefined)connect();
+  }, []);
+
   if (!session) return <Redirect href="/login" />;
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarStyle: { bottom: 0, height: 90, paddingTop: 10, backgroundColor: colorScheme === "dark" ? "#101010" : "#fff", borderTopWidth: 0, shadowColor: "rgba(0, 0, 0, 0.25)", shadowOffset: { width: 0, height: 1 }, shadowRadius: 9, shadowOpacity: 1 }
-      }}
-    >
-      <Tabs.Screen name="index" options={{ href: null }} />
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "chatbox" : "chatbox-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="car"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? "car" : "car-outline"} color={color} />
-          ),
-        }}
-      />
+    <OverlayProvider>
+      <Chat client={client}>
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            headerShown: false,
+            tabBarStyle: { bottom: 0, height: 90, paddingTop: 10, backgroundColor: colorScheme === "dark" ? "#101010" : "#fff", borderTopWidth: 0, shadowColor: "rgba(0, 0, 0, 0.25)", shadowOffset: { width: 0, height: 1 }, shadowRadius: 9, shadowOpacity: 1 }
+          }}
+        >
+          <Tabs.Screen name="index" options={{ href: null }} />
+          <Tabs.Screen
+            name="home"
+            options={{
+              title: "",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "home" : "home-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="chat"
+            options={{
+              title: "",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "chatbox" : "chatbox-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="car"
+            options={{
+              title: "",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon name={focused ? "car" : "car-outline"} color={color} />
+              ),
+            }}
+          />
 
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "settings" : "settings-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          href: null,
-          title: "",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+          <Tabs.Screen
+            name="settings"
+            options={{
+              title: "",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "settings" : "settings-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="explore"
+            options={{
+              href: null,
+              title: "",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tabs>
+      </Chat>
+    </OverlayProvider>
   );
 }
--- src/app/(tabs)/chat.tsx
+++ src/app/(tabs)/chat.tsx
@@ -1,13 +1,37 @@
+import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Channel, ChannelList, MessageInput, MessageList, useChatContext } from "stream-chat-expo";
+import { Channel as ChannelType } from "stream-chat";
+import Modal from "react-native-modal";
 import Button from "@/components/ui/Button";
-import { supabase } from "@/lib/supabase";
-import { useAuth } from "@/providers/AuthProvider";
-import { View, Text, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 const ChatScreen = () => {
+  const themeColor = useThemeColor({ light: "#151515", dark: "#fefefe" }, 'background');
+  const [channel, setChannel] = useState<ChannelType | null>();
+
+  const { client } = useChatContext();
+
+  if (channel) {
+    return (
+      <Modal isVisible style={{margin: 0, paddingBottom: 48, backgroundColor: "white"}} >
+        <Button mod={["white", "square"]} onPress={() => setChannel(null)} style={{position: 'absolute', top: 64, left: 32, zIndex: 999}}>
+          <AntDesign name='arrowleft' size={24} color={themeColor}/>
+        </Button>
+        <Channel channel={channel}>
+          <MessageList/>
+          <MessageInput/>
+        </Channel>
+      </Modal>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Chat</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ChannelList onSelect={setChannel}/>
+    </ThemedView>
   );
 };
 
@@ -15,7 +39,7 @@ export default ChatScreen;
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 24,
-    marginTop: 50,
+    height: "100%",
+    paddingTop: 50,
   },
 });
