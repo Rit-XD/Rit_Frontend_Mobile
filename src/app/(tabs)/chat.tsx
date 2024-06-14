ac67bbb5120c92643d8c1a6107cae01043ef1a9a
Senne <>
2024-06-14 14:24:38 +0200

    fix chat

--- src/app/(tabs)/chat.tsx
+++ src/app/(tabs)/chat.tsx
@@ -1,7 +1,12 @@
 import { ThemedView } from "@/components/ThemedView";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Channel, ChannelList, MessageInput, MessageList } from "stream-chat-expo";
+import {
+  Channel,
+  ChannelList,
+  MessageInput,
+  MessageList,
+} from "stream-chat-expo";
 import { Channel as ChannelType } from "stream-chat";
 import Modal from "react-native-modal";
 import Button from "@/components/ui/Button";
@@ -11,23 +16,35 @@ import { ThemedText } from "@/components/ThemedText";
 import { useAuth } from "@/providers/AuthProvider";
 import { Image } from "expo-image";
 
+const DateRemover = () => <></>;
+
 const ChatScreen = () => {
   const themeColor = useThemeColor(
     { light: "#151515", dark: "#fefefe" },
     "background"
   );
+
+  const backgroundColor = useThemeColor(
+    { light: "#fefefe", dark: "#151515" },
+    "background"
+  );
   const { user } = useAuth();
   const [channel, setChannel] = useState<ChannelType | null>();
-  const [channelUser, setChannelUser] = useState<{ name?: string; online?: boolean, image?: any, last_active?: Date }>({});
+  const [channelUser, setChannelUser] = useState<{
+    name?: string;
+    online?: boolean;
+    image?: any;
+    last_active?: Date;
+  }>({});
 
   const timeAgo = (date?: Date) => {
-    if (!date) return 'Onbekend';
+    if (!date) return "Onbekend";
     const now = new Date();
     const diff = now.getTime() - date.getTime(); // Tijdsverschil in milliseconden
     const minutes = Math.floor(diff / 60000);
     const hours = Math.floor(diff / 3600000);
     const days = Math.floor(diff / 86400000);
-  
+
     if (days > 0) {
       return `${days}d geleden`;
     } else if (hours > 0) {
@@ -35,7 +52,7 @@ const ChatScreen = () => {
     } else if (minutes > 0) {
       return `${minutes}m geleden`;
     } else {
-      return 'Zojuist';
+      return "Zojuist";
     }
   };
 
@@ -48,9 +65,9 @@ const ChatScreen = () => {
           online: !!user.user!.online,
           image: user.user!.image,
           last_active: new Date(user.user!.last_active!),
-        }))[1],
+        }))[1]
       );
-      channel.watch({ presence: true })
+      channel.watch({ presence: true });
     };
 
     updateChannelUsers();
@@ -58,24 +75,54 @@ const ChatScreen = () => {
 
   return (
     <>
-      {channel? (
-      <Modal isVisible style={{margin: 0, paddingBottom: 48, backgroundColor: "white"}} animationIn="slideInRight" animationOut="slideOutRight">
-          <View style={{marginTop: 50}}>
-          <Channel channel={channel}>
-            <View style={{flexDirection: "row", alignItems: "center", justifyContent: "flex-start", gap: 16, marginHorizontal: 24}}>
-          <Button mod={["white", "square"]} onPress={() => setChannel(null)}>
-            <AntDesign name='arrowleft' size={24} color={themeColor}/>
-          </Button>
-              {channelUser?.image? <Image source={channelUser.image} style={{height: 48, width: 48, borderRadius: 50}}></Image> : null}
-              <View style={{alignItems: "flex-start"}}>
-              <ThemedText style={styles.chatTitle}>{channelUser?.name? channelUser.name : "User"}</ThemedText>
-              <ThemedText style={styles.activeTitle}>{channelUser?.online? "Nu actief" : `${timeAgo(channelUser?.last_active)} actief`}</ThemedText>
-
+      {channel ? (
+        <Modal
+          isVisible
+          style={{
+            margin: 0,
+            paddingBottom: 48,
+            backgroundColor: backgroundColor,
+          }}
+          animationIn="slideInRight"
+          animationOut="slideOutRight"
+        >
+          <View style={{ marginTop: 50 }}>
+            <Channel channel={channel}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                  gap: 16,
+                  marginHorizontal: 24,
+                }}
+              >
+                <Button
+                  mod={["white", "square"]}
+                  onPress={() => setChannel(null)}
+                >
+                  <AntDesign name="arrowleft" size={24} color={themeColor} />
+                </Button>
+                {channelUser?.image ? (
+                  <Image
+                    source={channelUser.image}
+                    style={{ height: 48, width: 48, borderRadius: 50 }}
+                  ></Image>
+                ) : null}
+                <View style={{ alignItems: "flex-start" }}>
+                  <ThemedText style={styles.chatTitle}>
+                    {channelUser?.name ? channelUser.name : "User"}
+                  </ThemedText>
+                  <ThemedText style={styles.activeTitle}>
+                    {channelUser?.online
+                      ? "Nu actief"
+                      : `${timeAgo(channelUser?.last_active)} actief`}
+                  </ThemedText>
+                </View>
               </View>
-            </View>
-            <MessageList/>
-            <MessageInput/>
-          </Channel>
+              <MessageList DateHeader={DateRemover} />
+              <MessageInput />
+            </Channel>
           </View>
         </Modal>
       ) : (
@@ -105,19 +152,19 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 32,
     marginBottom: 24,
-    fontFamily: "Cocon"
+    fontFamily: "Cocon",
   },
   chatTitle: {
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
-    fontFamily: "Cocon"
-  }, 
+    fontFamily: "Cocon",
+  },
   activeTitle: {
     fontSize: 16,
     textAlign: "center",
-    fontFamily: "inter"
-  }, 
+    fontFamily: "inter",
+  },
   container: {
     height: "100%",
     paddingTop: 50,
--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -1,52 +1,123 @@
 import { primaryColor } from "@/constants/Colors";
 import { PropsWithChildren, useEffect, useState } from "react";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, useColorScheme } from "react-native";
 import { StreamChat } from "stream-chat";
-import { Chat, OverlayProvider, Streami18n } from "stream-chat-expo";
+import {
+  Chat,
+  DeepPartial,
+  OverlayProvider,
+  Streami18n,
+  Theme,
+} from "stream-chat-expo";
 import { useAuth } from "./AuthProvider";
-
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY!);
-const streami18n = new Streami18n({language: "nl"});
+const streami18n = new Streami18n({ language: "nl" });
 
-export default function ChatProvider({children}: PropsWithChildren) {
-    const [isReady, setIsReady] = useState(false);
-    const { user } = useAuth();
+export default function ChatProvider({ children }: PropsWithChildren) {
+  const [isReady, setIsReady] = useState(false);
+  const { user } = useAuth();
+  const authColorScheme = useAuth().colorScheme;
+  const colorScheme = useColorScheme();
 
-    useEffect(() => {
-        const connect = async() => {
-            await client.connectUser(
-              {
-                id: user!.id,
-                name: `${user!.firstname} ${user!.lastname}`,
-                image: user!.picture || 'https://i.imgur.com/fR9Jz14.png',
-              },
-              client.devToken(user!.id),
-            );
-            setIsReady(true);
-    
-          // const channel = client.channel('messaging', 'the_park', {
-          //   name: 'The Park',
-          // });
-          // await channel.watch();
-        }
-        connect();
+  const theme: DeepPartial<Theme> = {
+    colors: {
+      accent_blue: primaryColor,
+      overlay: primaryColor,
+      grey_gainsboro: colorScheme === "dark" ? "#303030" : "#fefefe",
+      white:
+        authColorScheme === "dark"
+          ? "#151515"
+          : authColorScheme === "light"
+          ? "#fefefe"
+          : useThemeColor({ light: "#fefefe", dark: "#151515" }, "background"),
+      black:
+        authColorScheme === "dark"
+          ? "#fefefe"
+          : authColorScheme === "light"
+          ? "#151515"
+          : useThemeColor({ light: "#151515", dark: "#fefefe" }, "background"),
+    },
+    messageList: {
+      container: {
+        backgroundColor: colorScheme === "dark" ? "#151515" : "#fefefe",
+      },
+    },
+    channelListMessenger: {
+      flatListContent: {
+        backgroundColor: colorScheme === "dark" ? "#151515" : "#fefefe",
+      },
+    },
+    channelPreview: {
+      container: {
+        backgroundColor: colorScheme === "dark" ? "#151515" : "#fefefe",
+      },
+    },
+    messageSimple: {
+      container: {
+        backgroundColor: colorScheme === "dark" ? "#151515" : "#fefefe",
+      },
+      content: {
+        senderMessageBackgroundColor: primaryColor,
+      },
+      gallery: {
+        galleryContainer: {
+          backgroundColor: colorScheme === "dark" ? "#151515" : "#fefefe",
+        },
+      },
+    },
+    messageInput: {
+      container: {
+        backgroundColor: colorScheme === "dark" ? "#151515" : "#fefefe",
+      },
+    },
+  };
 
-        return () => {
-          client.disconnectUser();
-          setIsReady(false);
-        }
-      }, []);
+  const style = { style: theme };
 
-    if(!isReady) return <ActivityIndicator style={{marginTop: 75}} color={primaryColor} size={"large"}/>;
+  useEffect(() => {
+    const connect = async () => {
+      await client.connectUser(
+        {
+          id: user!.id,
+          name: `${user!.firstname} ${user!.lastname}`,
+          image: user!.picture || "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken(user!.id)
+      );
 
+      setIsReady(true);
+
+      // const channel = client.channel('messaging', 'the_park', {
+      //   name: 'The Park',
+      // });
+      // await channel.watch();
+    };
+    connect();
+
+    return () => {
+      client.disconnectUser();
+      setIsReady(false);
+    };
+  }, []);
+
+  if (!isReady)
     return (
-        <>
-            <OverlayProvider i18nInstance={streami18n}>
-                <Chat client={client} i18nInstance={streami18n}>
-                    {children}
-                </Chat>
-            </OverlayProvider>
-        </>
-    )
+      <ActivityIndicator
+        style={{ marginTop: 75 }}
+        color={primaryColor}
+        size={"large"}
+      />
+    );
+
+  return (
+    <>
+      <OverlayProvider i18nInstance={streami18n} value={style}>
+        <Chat client={client} i18nInstance={streami18n}>
+          {children}
+        </Chat>
+      </OverlayProvider>
+    </>
+  );
 }
